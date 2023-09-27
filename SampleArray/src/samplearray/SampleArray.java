40c92db6853f1d961225136a569e8c05355b6ac6
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 16:36:41 +0100

    Add files via upload
    Updated

---
+++ SampleArray/src/samplearray/SampleArray.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package samplearray;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class SampleArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int x [] = new int [5];
+    x [0] = 10;
+    x [1] = 20;
+    x [2] = 30;
+    x [3] = 40;
+    x [4] = 100;
+for(int i =0; i<x.length; i++)
+System.out.print(x[i]);
+ }
+    }
+    
+
